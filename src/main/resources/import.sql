545f212e49e3ddfd9db8af5c7e9e7c01887b1403
KpK36 <kayopatryck1@gmail.com>
2023-07-21 20:36:48 -0300

    Seeding gradual base de dados tb_participante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,8 @@ INSERT INTO tb_categoria (descricao) VALUES ('Oficina')
 
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0, 1)
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.0, 2)
+
+INSERT INTO tb_participante (nome, email) VALUES ('José Silva', 'jose@gmail.com')
+INSERT INTO tb_participante (nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com')
+INSERT INTO tb_participante (nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com')
+INSERT INTO tb_participante (nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com')
