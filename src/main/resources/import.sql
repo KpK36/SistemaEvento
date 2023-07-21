b3a658fe493e745c60d97b5c4a2db34e425e9bb1
KpK36 <kayopatryck1@gmail.com>
2023-07-21 20:27:20 -0300

    Seeding gradual base de dados tb_atividade ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_categoria (descricao) VALUES ('Curso')
 INSERT INTO tb_categoria (descricao) VALUES ('Oficina')
 
-INSERT INTO tb_atividade (nome, descricao, preco) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0)
-INSERT INTO tb_atividade (nome, descricao, preco) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.0)
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0, 1)
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.0, 2)
